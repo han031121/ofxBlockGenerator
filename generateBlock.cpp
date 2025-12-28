4fcae4f130783565950339c9058f64f318fbf095
han031121 <han031121@gmail.com>
2025-12-28 22:14:28 +0900

    implement constructor and print function

--- generateBlock.cpp
+++ generateBlock.cpp
@@ -1,11 +1,29 @@
 #include "generateBlock.h"
 
-int main() {
-    int i1, i2, i3, i4;
-    std::cin >> i1 >> i2 >> i3 >> i4;
+void blockData::generateBlock() {
 
-    blockData b(i1, i2, i3, i4);
+}
+
+void blockData::printData() {
+    int tmp[MAX_SIZE][MAX_SIZE] = {0};
+    int mid = MAX_SIZE / 2;
+
+    for(Block b : data)
+        tmp[b.first.first][b.first.second] = b.second;
+
+    for(int i = mid - max_r; i <= mid + max_r; i++) {
+        for(int j = mid - max_c; j <= mid + max_c; j++)
+            std::cout << tmp[i][j] << " ";
+        std::cout << "\n";
+    }
+}
+
+int main() {
+    int i1, i2, i3, i4, i5; //block_count_1, block_count_2, max_r, max_c, max_h
+    std::cin >> i1 >> i2 >> i3 >> i4 >> i5;
+
+    blockData b(i1, i2, i3, i4, i5);
     b.generateBlock();
-    b.printResult();
+    b.printData();
     return 0;
 }
--- generateBlock.h
+++ generateBlock.h
@@ -6,24 +6,35 @@
 #include <ctime>
 #include <cstdlib>
 
-typedef std::pair<std::pair<int,int>, int> block;
-typedef std::vector<block> block_list;
+#define MAX_SIZE 17
+
+typedef std::pair<std::pair<int,int>, int> Block; //{position, height}
+typedef std::vector<Block> BlockList;
 
 class blockData {
     private:
-        int block_count;
-        int max_r;
-        int max_c;
+        std::pair<int,int> block_count; //max : max_r * max_c * max_h
+        int max_r; //max : MAX_SIZE / 2
+        int max_c; //max : MAX_SIZE / 2
         int max_h;
+        
+        int created_count[MAX_SIZE][MAX_SIZE][MAX_SIZE];
+        BlockList data = {};
 
-        double weight[100][100][100] = {0};
-
-        void setWeight();
-        double getWeight(int r, int c, int h);
+        double setWeight(int r, int c, int h); //calculate weight of specific position
 
     public:
-        blockData(int _block_count, int _max_r, int _max_c, int _max_h);
+        blockData(int _bc1, int _bc2, int _max_r, int _max_c, int _max_h)
+            : max_r(_max_r), max_c(_max_c), max_h(_max_h) {
+                block_count = {std::min(_bc1, _bc2), std::max(_bc1, _bc2)};
 
-        block_list generateBlock();
-        void printResult();
+                if(std::max(_bc1, _bc2) > max_r * max_c * max_h)
+                    throw std::length_error("block_count is too big");
+                if (_max_r > MAX_SIZE/2 || _max_c > MAX_SIZE/2)
+                    throw std::out_of_range("range error");
+            }
+
+        void generateBlock(); //make new Block data
+        void printData();
+        const BlockList& getData() const { return data; } //get current Block data
 };
