766de2a4da043ef47c2b043277192c305a7ab3c5
han031121 <han031121@gmail.com>
2026-02-07 01:06:14 +0900

    seperate generateBlock.cpp

--- makefile
+++ makefile
@@ -7,7 +7,8 @@ BUILD_DIR = build
 SRCS = $(SRC_DIR)/main.cpp \
        $(SRC_DIR)/generateBlock.cpp \
        $(SRC_DIR)/setWeight.cpp \
-       $(SRC_DIR)/utility.cpp
+       $(SRC_DIR)/utility.cpp \
+	   $(SRC_DIR)/setStatus.cpp
 
 OBJS = $(SRCS:$(SRC_DIR)/%.cpp=$(BUILD_DIR)/%.o)
 TARGET = $(BUILD_DIR)/main.exe
--- src/generateBlock.cpp
+++ src/generateBlock.cpp
@@ -75,21 +75,6 @@ void blockData::generateBlock() {
     convertBlockData();
 }
 
-void blockData::init() {
-    data = {};
-    std::fill(&cubic_data[0][0][0], &cubic_data[0][0][0] + MAX_SIZE*MAX_SIZE*MAX_SIZE, 0);
-    std::fill(&height_data[0][0], &height_data[0][0] + MAX_SIZE*MAX_SIZE, 0);
-
-    biggest_r = 0;
-    biggest_c = 0;
-    biggest_h = 0;
-    smallest_r = MAX_SIZE;
-    smallest_c = MAX_SIZE;
-    size_r = 0;
-    size_c = 0;
-    size_h = 0;
-}
-
 void blockData::convertBlockData() {
     for(int i = 0; i < max_r; i++) {
         for(int j = 0; j < max_c; j++) {
@@ -102,16 +87,4 @@ void blockData::convertBlockData() {
             }
         }
     }
-}
-
-void blockData::measureSize(Tuple added) {
-    biggest_r = std::max(get<0>(added), biggest_r);
-    biggest_c = std::max(get<1>(added), biggest_c);
-    biggest_h = std::max(get<2>(added), biggest_h);
-    smallest_r = std::min(get<0>(added), smallest_r);
-    smallest_c = std::min(get<1>(added), smallest_c);
-
-    size_r = biggest_r - smallest_r + 1;
-    size_c = biggest_c - smallest_c + 1;
-    size_h = biggest_h;
 }
